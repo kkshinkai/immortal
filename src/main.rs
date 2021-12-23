839cf644ffb9814bdda9d4eacae00029c650e01e
Kk Shinkai <kkshinkai@outlook.com>
2021-12-23 16:57:18 +0800

    Re-design the APIs of the symbol and symbol table

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,13 @@
-use crate::symbol_table::SymbolTable;
+use crate::symbol::Symbol;
 
-mod symbol_table;
 mod symbol;
 
 fn main() {
-    let s1 = SymbolTable::of("a".to_string());
-    let s2 = SymbolTable::of("b".to_string());
-    let s3 = SymbolTable::of("c".to_string());
-    let s4 = SymbolTable::of("a".to_string());
+    let s1 = Symbol::from("a".to_string());
+    let s2 = Symbol::from("b".to_string());
+    let s3 = Symbol::from("c".to_string());
+    let s4 = Symbol::from("a".to_string());
 
-    println!("s1 = {}, s2 = {}, s3 = {}, s4 = {}",
-             SymbolTable::get(s1), SymbolTable::get(s2), SymbolTable::get(s3), SymbolTable::get(s4));
+    println!("s1 = {}, s2 = {}, s3 = {}, s4 = {}", s1.to_string(), s2.to_string(), s3.to_string(), s4.to_string());
     println!("s1.id = {}, s2.id = {}, s3.id = {}, s4.id = {}", s1.id(), s2.id(), s3.id(), s4.id());
 }
--- src/symbol.rs
+++ src/symbol.rs
@@ -1,7 +1,64 @@
+use std::collections::HashMap;
+use std::sync::Once;
+
 #[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug)]
 pub struct Symbol(u32);
 
 impl Symbol {
-    pub fn new(id: u32) -> Self { Self(id) }
+    fn new(id: u32) -> Self { Self(id) }
     pub fn id(self) -> u32 { self.0 }
+
+    pub fn from(string: String) -> Symbol {
+        SymbolTable::default().intern(string)
+    }
+
+    pub fn to_string(self) -> &'static str {
+        SymbolTable::default().store[self.id() as usize].as_str()
+    }
+}
+
+struct SymbolTable {
+    table: HashMap<&'static String, Symbol>,
+    store: Vec<String>,
+}
+
+impl SymbolTable {
+    fn new() -> Self { Self::with_capacity(4096) }
+
+    fn with_capacity(capacity: usize) -> Self {
+        let capacity = capacity.next_power_of_two();
+        Self {
+            table: HashMap::with_capacity(capacity),
+            store: Vec::with_capacity(capacity),
+        }
+    }
+
+    fn intern(&mut self, contents: String) -> Symbol {
+        if let Some(&id) = self.table.get(&contents) {
+            return id
+        }
+
+        let id = Symbol::new(self.table.len() as u32);
+
+        self.table.insert(unsafe {
+            let ptr: *const String = &contents;
+            &*ptr
+        } as &'static _, id);
+        self.store.push(contents);
+        id
+    }
+}
+
+static mut SYMBOL_TABLE: Option<SymbolTable> = None;
+static ONCE: Once = Once::new();
+
+impl SymbolTable {
+    fn default() -> &'static mut SymbolTable {
+        unsafe {
+            ONCE.call_once(|| {
+                SYMBOL_TABLE = Some(SymbolTable::new());
+            });
+            SYMBOL_TABLE.as_mut().unwrap()
+        }
+    }
 }
